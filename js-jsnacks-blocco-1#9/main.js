7d86798f384a1292323adf8e425dbec5c88ce559
PierluigiBarocci <56305907+PierluigiBarocci@users.noreply.github.com>
2019-11-20 16:16:42 +0100

    Snack 9 - Terminato

--- js-jsnacks-blocco-1#9/main.js
+++ js-jsnacks-blocco-1#9/main.js
@@ -1,1 +1,8 @@
-//Calcola la somma e la media dei primi 10 numeri. 
+//Calcola la somma e la media dei primi 10 numeri.
+var sum = 0
+for (var i = 1; i <= 10; i++) {
+    sum = sum + i;
+}
+var average = (sum / 10);
+console.log('La somma tra i primi dieci numeri (escluso lo zero) è ' + sum);
+console.log('La loro media è ' + average);
